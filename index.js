95b0a08a3cdec28dc803775636155a8249c46263
Maharshi Vaghela <53486749+maharshi1712@users.noreply.github.com>
2021-02-28 21:53:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var color = 1;
+$('.btn-theme').click(function() {
+    if (color === 1) {
+        $('#container').css("background", "linear-gradient(to top, #8e9eab, #eef2f3)");
+        $('#calculator').css("color", "#272727");
+        $('.fa-moon').css("color", "#272727");
+        $('.fa-bars').css("color", "#272727");
+        $('.upperValue').css("color", "#272727");
+        $('.lowerValue').css("color", "#272727");
+        $('.number').css("color", "#363636");
+        $('.top-section,.side-section').css("background", "lightgrey");
+        $('.operator').css("background-color", "lightgrey");
+        $('.equal').css("background-color", "#FF6A00");
+        $('.number').css("background-color", "lightgrey");
+        $('body').css("background", "#f5f6f7");
+        $('#container').css("box-shadow", "-4px -2px 4px 0px #ffffff, 4px 2px 6px 0px #DFE4EA");
+        color = 0;
+    } else {
+        $('#container').css("background", "linear-gradient(to top, #000000, #434343)");
+        $('#calculator').css("color", "aliceblue");
+        $('.fa-moon').css("color", "#FF6A00");
+        $('.fa-bars').css("color", "#f0f0f0");
+        $('.upperValue').css("color", "#ffffff");
+        $('.lowerValue').css("color", "#ffffff");
+        $('.number').css("color", "#ffffff");
+        $('.top-section,.side-section').css("background", "#131419");
+        $('.operator').css("background-color", "#101115");
+        $('.equal').css("background-color", "#FF6A00");
+        $('.number').css("background-color", "#101115");
+        $('body').css("background", "#131419");
+        $('#container').css("box-shadow", "-3px -3px 10px rgba(255, 255, 255, .05), 3px 3px 15px rgba(0, 0, 0, .5)");
+        color = 1;
+    }
+})
