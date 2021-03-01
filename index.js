cea733ea1b2f8cb99c2d312b6335c96c9bad6f9c
Maharshi Vaghela <53486749+maharshi1712@users.noreply.github.com>
2021-03-01 11:14:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var color = 1;
 $('.btn-theme').click(function() {
     if (color === 1) {
-        $('#container').css("background", "linear-gradient(to top, #8e9eab, #eef2f3)");
+        $('#container').css("background", "#f5f6f7");
         $('#calculator').css("color", "#272727");
         $('.fa-moon').css("color", "#272727");
         $('.fa-bars').css("color", "#272727");
         $('.upperValue').css("color", "#272727");
         $('.lowerValue').css("color", "#272727");
         $('.number').css("color", "#363636");
-        $('.top-section,.side-section').css("background", "lightgrey");
-        $('.operator').css("background-color", "lightgrey");
+        $('.top-section,.side-section').css("background", "#f5f6f7");
+        $('.operator').css("background-color", "#f5f6f7");
         $('.equal').css("background-color", "#FF6A00");
-        $('.number').css("background-color", "lightgrey");
+        $('.number').css("background-color", "#f5f6f7");
         $('body').css("background", "#f5f6f7");
         $('#container').css("box-shadow", "-4px -2px 4px 0px #ffffff, 4px 2px 6px 0px #DFE4EA");
+        $('side-section').css("background", "#f5f6f7");
         color = 0;
     } else {
         $('#container').css("background", "linear-gradient(to top, #000000, #434343)");
@@ -31,4 +32,4 @@ $('.btn-theme').click(function() {
         $('#container').css("box-shadow", "-3px -3px 10px rgba(255, 255, 255, .05), 3px 3px 15px rgba(0, 0, 0, .5)");
         color = 1;
     }
-})
+});
